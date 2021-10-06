5d74622b5625d1fa3ba51f2dd742e00531f9a2b3
qiu <singleqaq@gmail.com>
2021-10-06 13:39:57 +0800

    :new: spring cloud gateway网关

---
+++ lab9-spring-cloud-gateway/nacos-discovery-gateway/src/main/java/io/github/qaqrose/nacos/discovery/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package io.github.qaqrose.nacos.discovery.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: qiu
+ * 2021/10/5
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
